19a4984b73f6196e06c62ed821d3b4c4eb3a1549
Touhid Mia <hmtm.cse@gmail.com>
2020-04-04 00:13:01 +0600

    Added 404 Page configuration

--- spring-boot-app/src/main/groovy/com/hmtmcse/text2web/controllers/SiteController.groovy
+++ spring-boot-app/src/main/groovy/com/hmtmcse/text2web/controllers/SiteController.groovy
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest
 @Controller
 class SiteController {
 
-    @RequestMapping("/{start:^(?!asset|static-files|internal-asset|text2web-manage|text2web-system).*}/**")
+    @RequestMapping("/{start:^(?!asset|static-files|internal-asset|text2web-manage|text2web-system|favicon\\.ico).*}/**")
     @ResponseBody
     String startWith(@PathVariable("start") String start, HttpServletRequest request) {
         TextToWebHtmlEngine templateEngine = new TextToWebHtmlEngine()
--- spring-boot-app/src/main/resources/templates/common/common-header.ftl
+++ spring-boot-app/src/main/resources/templates/common/common-header.ftl
@@ -1,4 +1,4 @@
-
+<link rel="icon" href="data:;base64,=">
 <link rel="stylesheet" href="/internal-asset/css/bootstrap.min.css">
 <link rel="stylesheet" href="/internal-asset/css/dashboard.css">
 <script src="/internal-asset/js/jquery-3.2.1.min.js"></script>
